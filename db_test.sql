54c41c68ab898f1075735a48c4b2e64f0b0c8a47
Tanya s <tanlull@gmail.com>
2022-06-17 22:53:56 +0700

    Migrate Data & Thai Support

---
+++ db_test.sql
@@ -0,0 +1,13 @@
+﻿create table province(
+ProvinceID INT ,
+ProvinceThai VARCHAR(200) NOT NULL,
+ProvinceEng VARCHAR(200) NOT NULL,
+Region VARCHAR(200) NOT NULL,  
+PRIMARY KEY (ProvinceID)
+); 
+INSERT INTO ProvinceDB.province (ProvinceID,ProvinceThai,ProvinceEng,Region) VALUES
+	 (10,'กรุงเทพมหานคร','Bangkok','ภาคกลาง'), 
+	 (11,'สมุทรปราการ','Samut Prakan','ภาคกลาง'),
+	 (12,'นนทบุรี','Nonthaburi','ภาคกลาง'),
+	 (13,'ปทุมธานี','Pathum Thani','ภาคกลาง'),
+	 (14,'พระนครศรีอยุธยา','Phra Nakhon Si Ayutthaya','ภาคกลาง');
