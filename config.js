1884c26d2d59a9d312d1cb0af2fc809ce11b51e4
Tanya s <tanlull@gmail.com>
2022-06-19 21:09:06 +0700

    add readme.md

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   db: {
     /* don't expose password or any sensitive info, done only for demo */
-    host: "localhost",
+    host: "tan-mysql-service",
     user: "root",
     password: "toor",
     database: "ProvinceDB",
